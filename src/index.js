c67956acbdf8055bd33cf016dc83acb5bf0d9932
DzmitryKas <Kasperovich_dima@mail.ru>
2019-03-19 09:46:21 +0300

    feat: implement a function that determines the minimum numbers of coins need to make changes to a certain amount in US currency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+nH = Math.floor (currency / 50);   
+nQ = Math.floor ((currency - (50 * nH )) / 25);
+nD = Math.floor ((currency - (50 * nH + 25 * nQ )) / 10);
+nN = Math.floor ((currency - (50 * nH + 25 * nQ + 10 * nD )) / 5);
+nP = Math.floor ((currency - (50 * nH + 25 * nQ + 10 * nD + 5 * nN )) / 1);
+
+ let Exchanger = {   
+ H : nH,  
+ Q : nQ,
+ D : nD,
+ N : nN,
+ P : nP,     
+};
+
+let ExchangerError = {
+  error : "You are rich, my friend! We don't have so much coins for exchange"
 }
+if (Exchanger.H < 1 ) {
+  delete Exchanger.H ;}
+
+if (Exchanger.Q < 1 ) {
+  delete Exchanger.Q ;}
+
+if (Exchanger.D < 1 ) {
+  delete Exchanger.D ;}
+
+if (Exchanger.N < 1 ) {
+  delete Exchanger.N ;}
+
+if (Exchanger.P < 1 ) {
+  delete Exchanger.P ;}
+
+
+if (currency > 9999) {
+return (ExchangerError);
+}  
+else { return (Exchanger) ; 
+  
+}
+}
+
